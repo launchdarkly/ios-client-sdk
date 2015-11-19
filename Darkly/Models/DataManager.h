f29905f5c8943640434ebe6f88ef39dd153e8518
Constantine Mavromoustakos <cmavromoustakos@gmail.com>
2015-11-18 23:18:03 -0500

    Event capacity
    Fixes #12
    
    https://github.com/launchdarkly/ios-client/issues/12

--- Darkly/Models/DataManager.h
+++ Darkly/Models/DataManager.h
@@ -13,23 +13,23 @@ extern int const kUserCacheSize;
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
--(void) saveContext;
--(NSManagedObjectContext *) managedObjectContext;
+@property (strong,atomic) NSNumber *eventCreatedCount;
 
 +(DataManager *)sharedManager;
 
+-(NSManagedObjectContext *) managedObjectContext;
 -(NSManagedObject *)findEvent: (NSInteger) date;
 -(NSData*) allEventsJsonData;
--(void) createFeatureEvent: (NSString *)featureKey keyValue:(BOOL)keyValue defaultKeyValue:(BOOL)defaultKeyValue;
--(void) createCustomEvent: (NSString *)eventKey
-withCustomValuesDictionary: (NSDictionary *)customDict;
 -(NSArray *)allEvents;
 -(Config *) createConfigFromJsonDict: (NSDictionary *)jsonConfigDictionary;
 -(UserEntity *)findUserEntityWithkey: (NSString *)key;
 -(User *)findUserWithkey: (NSString *)key;
--(void)purgeOldUsers;
--(void)deleteOrphanedConfig;
+-(void) createFeatureEvent: (NSString *)featureKey keyValue:(BOOL)keyValue defaultKeyValue:(BOOL)defaultKeyValue;
+-(void) createCustomEvent: (NSString *)eventKey withCustomValuesDictionary: (NSDictionary *)customDict;
+-(void) purgeOldUsers;
+-(void) deleteOrphanedConfig;
 -(void) saveUser: (User *) user;
+-(void) saveContext;
+-(void) deleteProcessedEvents: (NSArray *) processedJsonArray;
 
 @end
