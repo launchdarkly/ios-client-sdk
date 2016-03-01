2476d73ebd6b2fc31ea5330022a13d48185f5746
Constantinos Mavromoustakos <cmavromoustakos@Constantinoss-MacBook-Pro.local>
2016-03-01 08:38:13 -0500

    Stub darkly requests

--- DarklyTests/DarklyXCTestCase.m
+++ DarklyTests/DarklyXCTestCase.m
@@ -4,16 +4,30 @@
 
 #import "DarklyXCTestCase.h"
 #import "LDDataManager.h"
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHPathHelpers.h>
+#import <OCMock.h>
 
 @implementation DarklyXCTestCase
 
 - (void)setUp {
     [super setUp];
+    
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:@"foo" options: 0] ;
+    
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"app.launchdarkly.com"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithData: data statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+    }];
+    
+
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:kUserDictionaryStorageKey];
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:kEventDictionaryStorageKey];
 }
 
 - (void)tearDown {
+    [OHHTTPStubs removeAllStubs];
     [super tearDown];
 }
 
