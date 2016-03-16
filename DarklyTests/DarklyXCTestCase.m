3950fa3166404c32a224405b4461ea78f14c56a1
Jeff Byrnes <jnbyrnes@gmail.com>
2016-03-15 23:55:47 -0400

    initial commit

--- DarklyTests/DarklyXCTestCase.m
+++ DarklyTests/DarklyXCTestCase.m
@@ -4,7 +4,6 @@
 
 #import "DarklyXCTestCase.h"
 #import "LDDataManager.h"
-#import <OHHTTPStubs/OHHTTPStubs.h>
 #import <OHPathHelpers.h>
 #import <OCMock.h>
 
@@ -12,22 +11,12 @@
 
 - (void)setUp {
     [super setUp];
-    
-    NSData *data = [[NSData alloc] initWithBase64EncodedString:@"foo" options: 0] ;
-    
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        return [request.URL.host isEqualToString:@"app.launchdarkly.com"];
-    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
-        return [OHHTTPStubsResponse responseWithData: data statusCode:200 headers:@{@"Content-Type":@"application/json"}];
-    }];
-    
 
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:kUserDictionaryStorageKey];
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:kEventDictionaryStorageKey];
 }
 
 - (void)tearDown {
-    [OHHTTPStubs removeAllStubs];
     [super tearDown];
 }
 
--- DarklyTests/LDRequestManagerTest.m
+++ DarklyTests/LDRequestManagerTest.m
@@ -5,20 +5,48 @@
 #import "DarklyXCTestCase.h"
 #import "LDRequestManager.h"
 #import "LDDataManager.h"
+#import "LDClientManager.h"
+#import "LDUserBuilder.h"
+#import "LDConfig.h"
+#import "LDClient.h"
 #import <OCMock.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
 
 @interface LDRequestManagerTest : DarklyXCTestCase
+@property (nonatomic) id clientManagerMock;
+@property (nonatomic) id ldClientMock;
+@property (nonatomic) int tempFlushInterval;
 @end
 
 @implementation LDRequestManagerTest
+@synthesize clientManagerMock;
+@synthesize ldClientMock;
+@synthesize tempFlushInterval;
+
 - (void)setUp {
     [super setUp];
-
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    LDUserBuilder *userBuilder = [[LDUserBuilder alloc] init];
+    userBuilder = [userBuilder withKey:@"jeff@test.com"];
+    LDUserModel *user = [userBuilder build];
+    
+    LDConfigBuilder *configBuilder = [[LDConfigBuilder alloc] init];
+    tempFlushInterval = 30;
+    configBuilder = [configBuilder withFlushInterval:tempFlushInterval];
+    LDConfig *config = [configBuilder build];
+    
+    ldClientMock = OCMClassMock([LDClient class]);
+    OCMStub(ClassMethod([ldClientMock sharedInstance])).andReturn(ldClientMock);
+    OCMStub([ldClientMock ldUser]).andReturn(user);
+    OCMStub([ldClientMock ldConfig]).andReturn(config);
+    
+    clientManagerMock = OCMClassMock([LDClientManager class]);
+    OCMStub(ClassMethod([clientManagerMock sharedInstance])).andReturn(clientManagerMock);
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [OHHTTPStubs removeAllStubs];
     [super tearDown];
 }
 
@@ -28,6 +56,7 @@
     NSString *encodedUserString = @"eyJrZXkiOiAiamVmZkB0ZXN0LmNvbSJ9";
     LDRequestManager *requestManager = [LDRequestManager sharedInstance];
     [requestManager setApiKey:apiKey];
+    [requestManager setBaseUrl:kBaseUrl];
 
     BOOL requestInProgress = YES;
     [requestManager performFeatureFlagRequest:encodedUserString];
@@ -42,27 +71,26 @@
     
 }
 
-- (void)testEventRequest {
+- (void)testEventRequestForSuccess {
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:@"" options: 0] ;
+    
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+        return [request.URL.host isEqualToString:@"app.launchdarkly.com"];
+    } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+        return [OHHTTPStubsResponse responseWithData: data statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+    }];
     
     NSString *apiKey = @"YOUR_MOBILE_KEY";
-    
     NSString *jsonEventString = @"[{\"kind\": \"feature\", \"user\": {\"key\" : \"jeff@test.com\", \"custom\" : {\"groups\" : [\"microsoft\", \"google\"]}}, \"creationDate\": 1438468068, \"key\": \"isConnected\", \"value\": true, \"default\": false}]";
     NSData* eventData = [jsonEventString dataUsingEncoding:NSUTF8StringEncoding];
     
     LDRequestManager *requestManager = [LDRequestManager sharedInstance];
     [requestManager setApiKey:apiKey];
+    [requestManager setBaseUrl:kBaseUrl];
+    [requestManager setConnectionTimeout:10];
     
-    BOOL requestInProgress = YES;
     [requestManager performEventRequest:eventData];
-    
-    NSDate *timeoutDate = [NSDate dateWithTimeIntervalSinceNow:1.0];
-    
-    while (requestInProgress == YES && ([timeoutDate timeIntervalSinceNow] > 0)) {
-        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.01, YES);
-        requestInProgress = [requestManager eventRequestInProgress];
-    }
-    XCTAssertFalse(requestInProgress);
-    
+    OCMVerify([clientManagerMock processedEvents:YES jsonEventArray:eventData eventIntervalMillis:tempFlushInterval * kMillisInSecs]);
 }
 
 @end
