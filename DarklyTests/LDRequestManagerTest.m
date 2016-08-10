9c619d5112570f8d664b5d066d0a324ac73fb8d2
Danial Zahid <danialzahid94@live.com>
2016-08-11 02:47:23 +0500

    Fix test cases

--- DarklyTests/LDRequestManagerTest.m
+++ DarklyTests/LDRequestManagerTest.m
@@ -56,7 +56,7 @@
     NSData *data = [[NSData alloc] initWithBase64EncodedString:@"" options: 0] ;
     
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        return [request.URL.host isEqualToString:@"app.launchdarkly.com"];
+        return [request.URL.host isEqualToString:@"mobile.launchdarkly.com"];
     } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
         httpRequestAttempted = YES;
         [responseArrived fulfill];
@@ -87,7 +87,7 @@
     NSData *data = [[NSData alloc] initWithBase64EncodedString:@"" options: 0] ;
     
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        return [request.URL.host isEqualToString:@"app.launchdarkly.com"];
+        return [request.URL.host isEqualToString:@"mobile.launchdarkly.com"];
     } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
         httpRequestAttempted = YES;
         [responseArrived fulfill];
