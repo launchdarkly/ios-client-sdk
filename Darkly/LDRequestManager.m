3ee9e410b83e4eae3feac38f985a88d1bb757c1f
Danial Zahid <danialzahid94@live.com>
2016-10-13 19:11:04 +0500

    Fixes for header values and dispatch

--- Darkly/LDRequestManager.m
+++ Darkly/LDRequestManager.m
@@ -56,75 +56,16 @@ static NSString * const kEventRequestCompletedNotification = @"event_request_com
                 requestUrl = [requestUrl stringByAppendingString:encodedUser];
                 
                 [manager GET:requestUrl parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-                    dispatch_async(dispatch_get_main_queue(),^{
-                        configRequestInProgress = NO;
-                        if (responseObject) {
-                            NSDictionary *headerDictionary = [responseObject allHeaderFields];
-                            int configMaxAge = 0;
-                            
-                            // need to process the Expires header first because the
-                            // max-age directive should override the Expires header, even if
-                            // Expires header is more restrictive.
-                            
-                            if ([headerDictionary valueForKey:@"Expires"] != nil)
-                            {
-                                // response comes in as Expires: Tue, 15 May 2008 07:19:00 GMT
-                                
-                                NSString *queryExpiresString =[headerDictionary valueForKey:@"Expires"];
-                                
-                                NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-                                [formatter setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
-                                [formatter setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss zzz"];
-                                
-                                NSDate *endDateTime = [formatter dateFromString:queryExpiresString];
-                                
-                                NSTimeInterval dateTime = [endDateTime timeIntervalSinceNow];    //Gets difference in seconds
-                                if (dateTime >= 0)
-                                    configMaxAge = dateTime;
-                                
-                                // 0 should be minimum for max age.
-                                if (configMaxAge < 0)
-                                    configMaxAge = 0;
-                                
-                            }
-                            if ([headerDictionary valueForKey:@"Cache-Control"] != nil)
-                            {
-                                // response comes in as max-age=x seconds...
-                                // loop through the string just in case there are extra variables in the cache-control
-                                // and max-age doesn't appear first.
-                                
-                                NSString *queryString =[headerDictionary valueForKey:@"Cache-Control"];
-                                
-                                NSMutableDictionary *cacheControlQueryStrings = [[NSMutableDictionary alloc] init];
-                                for (NSString *qs in [queryString componentsSeparatedByString:@" "]) {
-                                    // Get the parameter name
-                                    NSString *key = [[qs componentsSeparatedByString:@"="] objectAtIndex:0];
-                                    // Get the parameter value
-                                    NSString *value = [[qs componentsSeparatedByString:@"="] objectAtIndex:1];
-                                    
-                                    cacheControlQueryStrings[key] = value;
-                                    
-                                    if ([key isEqualToString:@"max-age"])
-                                    {
-                                        configMaxAge = [value intValue];
-                                        break;
-                                    }
-                                    
-                                }
-                            }
-                            
-                            [delegate processedConfig:YES jsonConfigDictionary:responseObject configIntervalMillis:configMaxAge*kMillisInSecs];
-                        } else {
-                            [delegate processedConfig:NO jsonConfigDictionary:nil configIntervalMillis:kMinimumPollingIntervalMillis];
-                        }
-                    });
-                } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                    dispatch_async(dispatch_get_main_queue(),^{
-                        configRequestInProgress = NO;
+                    configRequestInProgress = NO;
+                    if (responseObject) {
+                        [delegate processedConfig:YES jsonConfigDictionary:responseObject configIntervalMillis:kMinimumPollingIntervalMillis];
+                    } else {
                         [delegate processedConfig:NO jsonConfigDictionary:nil configIntervalMillis:kMinimumPollingIntervalMillis];
-                    });
+                    }
+                } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                    configRequestInProgress = NO;
+                    [delegate processedConfig:NO jsonConfigDictionary:nil configIntervalMillis:kMinimumPollingIntervalMillis];
                 }];
-                
             } else {
                 DEBUG_LOGX(@"RequestManager unable to sync config to server since no encodedUser");
             }
@@ -152,18 +93,17 @@ static NSString * const kEventRequestCompletedNotification = @"event_request_com
                 
                 [manager POST:requestUrl parameters:jsonEventArray progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
                     // Call delegate method
-                    dispatch_async(dispatch_get_main_queue(),^{
-                        eventRequestInProgress = NO;
-                        LDClient *client = [LDClient sharedInstance];
-                        LDConfig *config = client.ldConfig;
-                        [delegate processedEvents:YES jsonEventArray:jsonEventArray eventIntervalMillis:[config.flushInterval intValue] * kMillisInSecs];
-                    });
+                    
+                    eventRequestInProgress = NO;
+                    LDClient *client = [LDClient sharedInstance];
+                    LDConfig *config = client.ldConfig;
+                    [delegate processedEvents:YES jsonEventArray:jsonEventArray eventIntervalMillis:[config.flushInterval intValue] * kMillisInSecs];
                 } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
                     // Call delegate method
-                    dispatch_async(dispatch_get_main_queue(),^{
-                        eventRequestInProgress = NO;
-                        [delegate processedEvents:NO jsonEventArray:jsonEventArray eventIntervalMillis:kMinimumPollingIntervalMillis];
-                    });
+                    
+                    eventRequestInProgress = NO;
+                    [delegate processedEvents:NO jsonEventArray:jsonEventArray eventIntervalMillis:kMinimumPollingIntervalMillis];
+                    
                 }];
             } else {
                 DEBUG_LOGX(@"RequestManager unable to sync events to server since no events");
