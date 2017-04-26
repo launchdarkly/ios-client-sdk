57af8d067041fccaefc92131ac4eb437b646c48f
Korhan <korhan.bircan@capitalone.com>
2017-04-26 09:34:49 -0700

    Converted custom setters to class properties.

--- Darkly/LDConfig.h
+++ Darkly/LDConfig.h
@@ -4,99 +4,81 @@
 
 @interface LDConfig : NSObject
 
+/**
+ This is the mobileKey retrieved from the Launch Darkly account settings.
+ */
 @property (nonatomic, readonly, nonnull) NSString* mobileKey;
+
+/**
+ The baseUrl of the Launch Darkly server. This will allow you to
+ switch between production and staging environments.
+ */
 @property (nonatomic, copy, nullable) NSString* baseUrl;
+
+/**
+ The eventsUrl of the Launch Darkly server. This will allow you
+ to switch between production and staging environments.
+ */
 @property (nonatomic, copy, nullable) NSString* eventsUrl;
+
+/**
+ The capacity for storing feature flag and custom events. Events
+ are persisted on the client and then synced to the server on a regular
+ basis. If there is ever a prolonged period of time between the last server
+ sync, the capacity defined here will determine at what points the events
+ are ignored and no longer stored. The default is 100.
+ */
 @property (nonatomic, copy, nullable) NSNumber* capacity;
+
+/**
+ The connection timeout to be used when syncing to the Launch Darkly
+ server. The default is 10 seconds.
+ */
 @property (nonatomic, copy, nullable) NSNumber* connectionTimeout;
+
+/**
+ The interval at which events are synced to the server. The default
+ is 30 seconds for streaming mode; in polling mode, the flush interval defaults
+ to the polling interval. (Optional)
+ */
 @property (nonatomic, copy, nullable) NSNumber* flushInterval;
+
+/**
+ The polling interval (in seconds) for polling mode only. An interval
+ less than 300 is set to the default (5 minutes).
+ */
 @property (nonatomic, copy, nullable) NSNumber* pollingInterval;
 
+/**
+ Initializes an LDConfig object with the provided mobile key.
+ @param mobileKey The mobileKey retrieved from the Launch Darkly account settings.
+ @return An instance of LDConfig object.
+ */
 - (instancetype _Nonnull)initWithMobileKey:(nonnull NSString *)mobileKey;
 
 @end
 
 @interface LDConfigBuilder : NSObject
 
+/**
+ Flag that enables streaming mode. When streaming is false, disable streaming
+ and switch to polling mode.
+ */
 @property (nonatomic) BOOL streaming;
+
+/**
+ Flat that enables debug mode to allow things such as logging.
+ */
 @property (nonatomic) BOOL debugEnabled;
 
+/**
+ Initializes an LDConfigBuilder object with the provided LDConfig object.
+
+ @param config LDConfig containing necessary information to initalize the builder.
+ @return An instance of LDConfigBuilder.
+ */
 - (instancetype _Nonnull)initWithConfig:(nonnull LDConfig *)config;
 
-/**
- * Provide an mobileKey to the configuration builder. This is the mobileKey
- * retrieved from the Launch Darkly account settings. (Required)
- *
- * @param mobileKey    the mobileKey for the configuration
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withMobileKey:(NSString *)mobileKey;
-/**
- * Provide the baseUrl of the Launch Darkly server. This will allow you
- * to switch between production and staging environments. (Optional)
- *
- * @param baseUrl    the baseUrl of the server
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withBaseUrl:(NSString *)baseUrl;
-/**
- * Provide the eventsUrl of the Launch Darkly server. This will allow you
- * to switch between production and staging environments. (Optional)
- *
- * @param eventsUrl    the eventsUrl of the server
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withEventsUrl:(NSString *)eventsUrl;
-/**
- * Provide the capacity for storing feature flag and custom events. Events
- * are persisted on the client and then synced to the server on a regular 
- * basis. If there is ever a prolonged period of time between the last server
- * sync, the capacity defined here will determine at what points the events
- * are ignored and no longer stored. The default is 100. (Optional)
- *
- * @param capacity  the number of events to store
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withCapacity:(int)capacity;
-/**
- * The connection timeout to be used when syncing to the Launch Darkly 
- * server. The default is 10 seconds. (Optional)
- *
- * @param connectionTimeout timeout for network connections in seconds
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withConnectionTimeout:(int)connectionTimeout;
-/**
- * The interval at which events are synced to the server. The default
- * is 30 seconds for streaming mode; in polling mode, the flush interval defaults to the polling interval. (Optional)
- *
- * @param flushInterval the flush interval in seconds
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withFlushInterval:(int)flushInterval;
-/**
- * Set the polling interval (in seconds) for polling mode only. An interval
- * less than 300 is set to the default (5 minutes).
- *
- * @param pollingInterval the polling interval in seconds
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withPollingInterval:(int)pollingInterval;
-/**
- * Enable streaming mode for flags. When streaming is false, disable streaming and switch to polling mode. (Optional)
- *
- * @param streamingEnabled Whether streaming is enabled or not
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withStreaming:(BOOL)streamingEnabled;
-/**
- * Enable debug mode to allow things such as logging. (Optional)
- *
- * @param debugEnabled Whether debugging is enabled or not
- * @return the configuration builder
- */
-- (LDConfigBuilder *)withDebugEnabled:(BOOL)debugEnabled;
-
--(LDConfig *)build;
+-(LDConfig * _Nonnull)build;
 
 @end
