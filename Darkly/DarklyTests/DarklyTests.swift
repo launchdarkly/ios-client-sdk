52adc5a297807350c098468813d85366b9478902
Mark Pokorny <markpokornycos@gmail.com>
2017-07-24 08:04:00 -0600

    initial swift repo setup

---
+++ Darkly/DarklyTests/DarklyTests.swift
@@ -0,0 +1,36 @@
+//
+//  DarklyTests.swift
+//  DarklyTests
+//
+//  Created by Mark Pokorny on 7/21/17.
+//  Copyright © 2017 LaunchDarkly. All rights reserved.
+//
+
+import XCTest
+@testable import Darkly
+
+class DarklyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
