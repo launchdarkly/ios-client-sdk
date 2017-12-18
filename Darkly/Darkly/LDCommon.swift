44da6405b9f00fcd4027eade4e77d406c847354a
Mark Pokorny <markpokornycos@gmail.com>
2017-12-18 08:51:42 -0700

    adds LDFlagKeyList.anyKey

--- Darkly/Darkly/LDCommon.swift
+++ Darkly/Darkly/LDCommon.swift
@@ -12,3 +12,8 @@ public typealias LDFlagChangeOwner = AnyObject
 public typealias LDFlagKeyList = [String]
 public typealias LDFlagChangeObserver = (LDChangedFlag) -> Void
 public typealias LDFlagCollectionChangeObserver = ([String: LDChangedFlag]) -> Void
+
+extension Array where Element == String {
+    private static var anyKeyIdentifier: String { return "Darkly.FlagKeyList.Any" }
+    static var anyKey: LDFlagKeyList { return [anyKeyIdentifier] }
+}
