a06316f9049f467349d0150715cf0ba84821a9b5
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-02-14 10:51:13 -0800

    Change minimum polling interval to 5 minutes

--- Darkly/Darkly/LDConfig.swift
+++ Darkly/Darkly/LDConfig.swift
@@ -13,42 +13,42 @@ public enum LDStreamingMode {
 }
 
 public struct LDConfig {
-    
+
     fileprivate struct Constants {
         static let millisPerSecond: Double = 1000
     }
-    
+
     fileprivate struct Defaults {
         static let baseUrl = URL(string: "https://app.launchdarkly.com")!
         static let eventsUrl = URL(string: "https://mobile.launchdarkly.com")!
         static let streamUrl = URL(string: "https://clientstream.launchdarkly.com")!  //Android SDK has this, ios has it hard-coded
         
         static let eventCapacity = 100
-        
+
         static let connectionTimeoutMillis = 10_000
         static let eventFlushIntervalMillis = 30_000    //Android SDK uses 5_000
         static let pollIntervalMillis = 300_000
         static let backgroundPollIntervalMillis = 3_600_000  //Android SDK uses 15m min, 1h default
-        
+
         static let streaming = LDStreamingMode.streaming
         static let enableBackgroundUpdates = true   //Android sdk has this config item
         static let online = true
-        
+
         static let debugMode = false
     }
-    
+
     public struct Minima {
         public let backgroundPollIntervalMillis = 900_000   ///The minimum background polling interval in milliseconds. Value: 15 minutes
-        public let pollingIntervalMillis = 60_000   ///The minimum polling interval in milliseconds. Value: 1 minute
+        public let pollingIntervalMillis = 300_000   ///The minimum polling interval in milliseconds. Value: 5 minutes
     }
-    
+
     public var baseUrl: URL = Defaults.baseUrl                ///You probably don't need to set this unless instructed by LaunchDarkly.
     public var eventsUrl: URL = Defaults.eventsUrl            ///You probably don't need to set this unless instructed by LaunchDarkly.
     public var streamUrl: URL = Defaults.streamUrl            ///You probably don't need to set this unless instructed by LaunchDarkly.
-    
+
     ///The max number of analytics events to queue before sending them to LaunchDarkly. Default: 100
     public var eventCapacity: Int = Defaults.eventCapacity
-    
+
     //Time configuration
     ///The timeout in milliseconds when connecting to LaunchDarkly. Default: 10 seconds
     public var connectionTimeoutMillis: Int = Defaults.connectionTimeoutMillis
@@ -83,10 +83,10 @@ public struct LDConfig {
 
     ///Enables additional logging for development. Default: false
     public var isDebugMode: Bool = Defaults.debugMode
-    
+
     ///LD defined minima
     public let minima = Minima()
-    
+
     public init() { }   //Even though the struct is public, the default constructor is internal
 
     func flagPollingInterval(runMode: LDClientRunMode) -> TimeInterval {
