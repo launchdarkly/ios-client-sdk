dc13c299e5ffa471ee0edd0852353750837807e9
Mark Pokorny <markpokornycos@gmail.com>
2018-03-28 10:26:58 -0600

    modifies umbrella header

--- Darkly/Darkly/Support/Darkly.h
+++ Darkly/Darkly/Support/Darkly.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 LaunchDarkly. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Darkly.
 FOUNDATION_EXPORT double DarklyVersionNumber;
