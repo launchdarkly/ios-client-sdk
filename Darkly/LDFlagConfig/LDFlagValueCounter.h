4ea0a6209ae658dde7986b00010f20f6206a4ada
Mark Pokorny <markpokornycos@gmail.com>
2018-04-18 11:02:24 -0600

    adds a new LDFlagConfig group and moves some files there

---
+++ Darkly/LDFlagConfig/LDFlagValueCounter.h
@@ -0,0 +1,13 @@
+//
+//  LDFlagValueCounter.h
+//  Darkly
+//
+//  Created by Mark Pokorny on 4/18/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LDFlagValueCounter : NSObject
+
+@end
---
+++ Darkly/LDFlagConfig/LDFlagValueCounter.m
@@ -0,0 +1,13 @@
+//
+//  LDFlagValueCounter.m
+//  Darkly
+//
+//  Created by Mark Pokorny on 4/18/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+#import "LDFlagValueCounter.h"
+
+@implementation LDFlagValueCounter
+
+@end
