b66e5eb0de074bb8c917f0c8ac020b9b13e37845
Mark Pokorny <markpokornycos@gmail.com>
2018-05-22 12:54:36 -0600

    add tooltip comments about ignoring optional items for equality checks

--- Darkly/LDFlagConfig/LDFlagConfigValue.h
+++ Darkly/LDFlagConfig/LDFlagConfigValue.h
@@ -18,9 +18,11 @@ extern NSString * _Nonnull const kLDFlagConfigValueKeyVariation;
 extern NSInteger const kLDFlagConfigValueItemDoesNotExist;
 
 @interface LDFlagConfigValue: NSObject
+//Core Items
 @property (nullable, nonatomic, strong) id value;
 @property (nonatomic, assign) NSInteger modelVersion;
 @property (nonatomic, assign) NSInteger variation;
+//Optional Items, excluded from equality tests
 @property (nullable, nonatomic, strong) NSNumber *flagVersion;
 @property (nullable, nonatomic, strong) LDEventTrackingContext *eventTrackingContext;
 
@@ -32,8 +34,12 @@ extern NSInteger const kLDFlagConfigValueItemDoesNotExist;
 -(nonnull NSDictionary*)dictionaryValue;
 -(nonnull NSDictionary*)dictionaryValueUseFlagVersionForVersion:(BOOL)useFlagVersion;
 
+///Returns true when the core items of both flagConfigValues are the same.
+///Ignores the optional items
 -(BOOL)isEqualToFlagConfigValue:(nullable LDFlagConfigValue*)other;
 -(BOOL)isEqual:(nullable id)object;
+///Returns true when the dictionary has the same core item values as the flagConfigValue
+///NOTE: Will return false if the dictionary was created with UseFlagVersionForVersion = YES and the flagVersion differs from the modelVersion
 -(BOOL)hasPropertiesMatchingDictionary:(nullable NSDictionary*)dictionary;
 
 -(nonnull NSString*)description;
