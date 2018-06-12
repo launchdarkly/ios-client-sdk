41cc10fec51991237f78a13bd4e7433d2757854b
Mark Pokorny <markpokornycos@gmail.com>
2018-06-12 11:37:13 -0600

    adds EventTrackingContext

---
+++ Darkly/Darkly/Models/EventTrackingContext.swift
@@ -0,0 +1,37 @@
+//
+//  EventTrackingContext.swift
+//  Darkly
+//
+//  Created by Mark Pokorny on 6/12/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+import Foundation
+
+struct EventTrackingContext {
+    enum CodingKeys: String, CodingKey {
+        case trackEvents
+    }
+
+    let trackEvents: Bool
+
+    init(trackEvents: Bool) {
+        self.trackEvents = trackEvents
+    }
+
+    init?(dictionary: [String: Any]) {
+        guard let trackEvents = dictionary.trackEvents else { return nil }
+        self.init(trackEvents: trackEvents)
+    }
+
+    init?(object: Any?) {
+        guard let dictionary = object as? [String: Any] else { return nil }
+        self.init(dictionary: dictionary)
+    }
+}
+
+extension Dictionary where Key == String, Value == Any {
+    var trackEvents: Bool? {
+        return self[EventTrackingContext.CodingKeys.trackEvents.rawValue] as? Bool
+    }
+}
---
+++ Darkly/DarklyTests/Models/EventTrackingContextSpec.swift
@@ -0,0 +1,103 @@
+//
+//  EventTrackingContextSpec.swift
+//  DarklyTests
+//
+//  Created by Mark Pokorny on 6/12/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import Darkly
+
+final class EventTrackingContextSpec: QuickSpec {
+
+    override func spec() {
+        initSpec()
+        dictionaryValueSpec()
+    }
+
+    private func initSpec() {
+        describe("init") {
+            var eventTrackingContext: EventTrackingContext!
+            it("creates a context with matching values") {
+                [true, false].forEach { (trackEvents) in
+                    eventTrackingContext = EventTrackingContext(trackEvents: trackEvents)
+
+                    expect(eventTrackingContext.trackEvents) == trackEvents
+                }
+            }
+        }
+
+        describe("init with dictionary") {
+            var eventTrackingDictionary: [String: Any]!
+            var eventTrackingContext: EventTrackingContext?
+            context("dictionary contains trackEvents") {
+                it("creates a context with matching values") {
+                    [true, false].forEach { (trackEvents) in
+                        eventTrackingDictionary = Dictionary(trackEvents: trackEvents)
+
+                        eventTrackingContext = EventTrackingContext(dictionary: eventTrackingDictionary)
+
+                        expect(eventTrackingContext?.trackEvents) == trackEvents
+                    }
+
+                }
+            }
+            context("dictionary omits trackEvents") {
+                beforeEach {
+                    eventTrackingDictionary = DarklyServiceMock.FlagValues.dictionary
+
+                    eventTrackingContext = EventTrackingContext(dictionary: eventTrackingDictionary)
+                }
+                it("returns nil") {
+                    expect(eventTrackingContext).to(beNil())
+                }
+            }
+        }
+
+        describe("init with object") {
+            var object: Any!
+            var eventTrackingContext: EventTrackingContext?
+            context("object is a dictionary containing trackEvents") {
+                beforeEach {
+                    object = Dictionary(trackEvents: true)
+
+                    eventTrackingContext = EventTrackingContext(object: object)
+                }
+                it("creates a context with matching values") {
+                    expect(eventTrackingContext?.trackEvents) == true
+                }
+            }
+            context("object is not a dictionary or does not contain trackEvents") {
+                it("returns nil") {
+                    DarklyServiceMock.FlagValues.all.forEach { (object) in
+                        eventTrackingContext = EventTrackingContext(object: object)
+
+                        expect(eventTrackingContext).to(beNil())
+                    }
+                }
+            }
+            context("object is nil") {
+                beforeEach {
+                    eventTrackingContext = EventTrackingContext(object: nil)
+                }
+                it("returns nil") {
+                    expect(eventTrackingContext).to(beNil())
+                }
+            }
+        }
+    }
+
+    private func dictionaryValueSpec() {
+
+    }
+}
+
+extension Dictionary where Key == String, Value == Any {
+    init(trackEvents: Bool) {
+        self.init()
+        self[EventTrackingContext.CodingKeys.trackEvents.rawValue] = trackEvents
+    }
+}
