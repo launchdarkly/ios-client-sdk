b18ccd9c06139b6c8feb51edb416c39fc47738f1
Mark Pokorny <markpokornycos@gmail.com>
2018-06-19 15:59:31 -0600

    add flagValueCounterSpec

--- Darkly/Darkly/Models/FeatureFlag/FlagRequestTracking/FlagValueCounter.swift
+++ Darkly/Darkly/Models/FeatureFlag/FlagRequestTracking/FlagValueCounter.swift
@@ -9,5 +9,33 @@
 import Foundation
 
 struct FlagValueCounter {
-    
+    enum CodingKeys: String, CodingKey {
+        case value, variation, version, unknown, count
+    }
+
+    let reportedValue: Any?
+    let featureFlag: FeatureFlag?
+    let isKnown: Bool
+    var count: Int
+
+    init(reportedValue: Any?, featureFlag: FeatureFlag?) {
+        self.reportedValue = reportedValue
+        self.featureFlag = featureFlag
+        isKnown = featureFlag != nil
+        count = 1
+    }
+
+    var dictionaryValue: [String: Any] {
+        var counterDictionary = [String: Any]()
+        counterDictionary[CodingKeys.value.rawValue] = reportedValue ?? NSNull()
+        counterDictionary[CodingKeys.count.rawValue] = count
+        if isKnown {
+            counterDictionary[CodingKeys.variation.rawValue] = featureFlag?.variation
+            counterDictionary[CodingKeys.version.rawValue] = featureFlag?.flagVersion ?? featureFlag?.version
+        } else {
+            counterDictionary[CodingKeys.unknown.rawValue] = true
+        }
+
+        return counterDictionary
+    }
 }
---
+++ Darkly/DarklyTests/Models/FeatureFlag/FlagValueCounterSpec.swift
@@ -0,0 +1,120 @@
+//
+//  FlagValueCounterSpec.swift
+//  DarklyTests
+//
+//  Created by Mark Pokorny on 6/19/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import Darkly
+
+final class FlagValueCounterSpec: QuickSpec {
+    override func spec() {
+        initSpec()
+        dictionaryValueSpec()
+    }
+
+    private func initSpec() {
+        describe("init") {
+            var flagValueCounter: FlagValueCounter!
+            context("for known values") {
+                var featureFlags: [LDFlagKey: FeatureFlag]!
+                beforeEach {
+                    featureFlags = DarklyServiceMock.Constants.stubFeatureFlags()
+                }
+                it("creates a known value counter") {
+                    featureFlags.forEach { (_, featureFlag) in
+                        flagValueCounter = FlagValueCounter(reportedValue: featureFlag.value, featureFlag: featureFlag)
+
+                        expect(AnyComparer.isEqual(flagValueCounter.reportedValue, to: featureFlag.value)).to(beTrue())
+                        expect(flagValueCounter.featureFlag) == featureFlag
+                        expect(flagValueCounter.isKnown) == true
+                        expect(flagValueCounter.count) == 1
+                    }
+                }
+            }
+            context("for unknown values") {
+                beforeEach {
+                    flagValueCounter = FlagValueCounter(reportedValue: true, featureFlag: nil)
+                }
+                it("creates an unknown value counter") {
+                    expect(AnyComparer.isEqual(flagValueCounter.reportedValue, to: true)).to(beTrue())
+                    expect(flagValueCounter.featureFlag).to(beNil())
+                    expect(flagValueCounter.isKnown) == false
+                    expect(flagValueCounter.count) == 1
+                }
+            }
+        }
+    }
+
+    private func dictionaryValueSpec() {
+        describe("dictionaryValue") {
+            var flagValueCounter: FlagValueCounter!
+            var flagValueCounterDictionary: [String: Any]!
+            context("for known values") {
+                var featureFlags: [LDFlagKey: FeatureFlag]!
+                context("with flag version") {
+                    beforeEach {
+                        featureFlags = DarklyServiceMock.Constants.stubFeatureFlags()
+                    }
+                    it("creates a known value dictionary with elements matching the feature flag") {
+                        featureFlags.forEach { (_, featureFlag) in
+                            flagValueCounter = FlagValueCounter(reportedValue: featureFlag.value, featureFlag: featureFlag)
+
+                            flagValueCounterDictionary = flagValueCounter.dictionaryValue
+
+                            expect(AnyComparer.isEqual(flagValueCounterDictionary.valueCounterReportedValue, to: featureFlag.value, considerNilAndNullEqual: true)).to(beTrue())
+                            expect(flagValueCounterDictionary.valueCounterVariation) == featureFlag.variation
+                            expect(flagValueCounterDictionary.valueCounterVersion) == featureFlag.flagVersion
+                            expect(flagValueCounterDictionary.valueCounterIsUnknown).to(beNil())
+                            expect(flagValueCounterDictionary.valueCounterCount) == 1
+                        }
+                    }
+                }
+                context("without flag version") {
+                    beforeEach {
+                        featureFlags = DarklyServiceMock.Constants.stubFeatureFlags(includeFlagVersions: false)
+                    }
+                    it("creates a known value dictionary with elements matching the feature flag") {
+                        featureFlags.forEach { (_, featureFlag) in
+                            flagValueCounter = FlagValueCounter(reportedValue: featureFlag.value, featureFlag: featureFlag)
+
+                            flagValueCounterDictionary = flagValueCounter.dictionaryValue
+
+                            expect(AnyComparer.isEqual(flagValueCounterDictionary.valueCounterReportedValue, to: featureFlag.value, considerNilAndNullEqual: true)).to(beTrue())
+                            expect(flagValueCounterDictionary.valueCounterVariation) == featureFlag.variation
+                            expect(flagValueCounterDictionary.valueCounterVersion) == featureFlag.version
+                            expect(flagValueCounterDictionary.valueCounterIsUnknown).to(beNil())
+                            expect(flagValueCounterDictionary.valueCounterCount) == 1
+                        }
+                    }
+                }
+            }
+            context("for an unknown value") {
+                beforeEach {
+                    flagValueCounter = FlagValueCounter(reportedValue: true, featureFlag: nil)
+
+                    flagValueCounterDictionary = flagValueCounter.dictionaryValue
+                }
+                it("creates an unknown value dictionary") {
+                    expect(AnyComparer.isEqual(flagValueCounterDictionary.valueCounterReportedValue, to: true)).to(beTrue())
+                    expect(flagValueCounterDictionary.valueCounterVariation).to(beNil())
+                    expect(flagValueCounterDictionary.valueCounterVersion).to(beNil())
+                    expect(flagValueCounterDictionary.valueCounterIsUnknown) == true
+                    expect(flagValueCounterDictionary.valueCounterCount) == 1
+                }
+            }
+        }
+    }
+}
+
+extension Dictionary where Key == String, Value == Any {
+    var valueCounterReportedValue: Any? { return self[FlagValueCounter.CodingKeys.value.rawValue] }
+    var valueCounterVariation: Int? { return self[FlagValueCounter.CodingKeys.variation.rawValue] as? Int }
+    var valueCounterVersion: Int? { return self[FlagValueCounter.CodingKeys.version.rawValue] as? Int }
+    var valueCounterIsUnknown: Bool? { return self[FlagValueCounter.CodingKeys.unknown.rawValue] as? Bool }
+    var valueCounterCount: Int? { return self[FlagValueCounter.CodingKeys.count.rawValue] as? Int }
+}
