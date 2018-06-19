0af8aeb1a6a4264b4249d6b2f34b3814dd6a590b
Mark Pokorny <markpokornycos@gmail.com>
2018-06-19 14:46:51 -0600

    add FlagValueCounter
    regroup some existing files

---
+++ Darkly/Darkly/Models/FeatureFlag/FlagRequestTracking/FlagValueCounter.swift
@@ -0,0 +1,13 @@
+//
+//  FlagValueCounter.swift
+//  Darkly
+//
+//  Created by Mark Pokorny on 6/19/18. +JMJ
+//  Copyright © 2018 LaunchDarkly. All rights reserved.
+//
+
+import Foundation
+
+struct FlagValueCounter {
+    
+}
