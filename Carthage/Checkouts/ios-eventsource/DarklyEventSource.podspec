65e172398b564b2163478d5e3ac21071b12c75db
Mark Pokorny <markpokornycos@gmail.com>
2018-08-07 08:57:45 -0600

    update carthage build to DarklyEventSource 3.2.6

--- Carthage/Checkouts/ios-eventsource/DarklyEventSource.podspec
+++ Carthage/Checkouts/ios-eventsource/DarklyEventSource.podspec
@@ -1,12 +1,12 @@
 Pod::Spec.new do |s|
 	s.name         = "DarklyEventSource"
-	s.version      = "3.2.5"
+	s.version      = "3.2.6"
 	s.summary      = "HTML5 Server-Sent Events in your Cocoa app."
 	s.homepage     = "https://github.com/launchdarkly/ios-eventsource"
 	s.license      = 'MIT (see LICENSE.txt)'
 	s.author       = { "Neil Cowburn" => "git@neilcowburn.com" }
-	s.source       = { :git => "https://github.com/launchdarkly/ios-eventsource.git", :tag => '3.2.5' }
-	s.source_files = 'LDEventSource', 'LDEventSource/LDEventSource.{h,m}'
+	s.source       = { :git => "https://github.com/launchdarkly/ios-eventsource.git", :tag => '3.2.6' }
+	s.source_files = 'LDEventSource/**/*.{h,m}'
 	s.ios.deployment_target = '8.0'
 	s.osx.deployment_target = '10.10'
 	s.watchos.deployment_target = '2.0'
--- Carthage/Checkouts/ios-eventsource/LDEventSource/NSString+LDEventSource.m
+++ Carthage/Checkouts/ios-eventsource/LDEventSource/NSString+LDEventSource.m
@@ -8,9 +8,30 @@
 
 #import "NSString+LDEventSource.h"
 
+NSString *const LDEventSourceKeyValueDelimiter = @":";
+
+NSString *const LDEventKeyData = @"data";
+NSString *const LDEventKeyId = @"id";
+NSString *const LDEventKeyEvent = @"event";
+NSString *const LDEventKeyRetry = @"retry";
+
+NSString * const LDEventSourceEventTerminator = @"\n\n";
+
 @implementation NSString(LDEventSource)
 -(NSArray<NSString*>*)lines {
     if (self.length == 0) { return nil; }
     return [self componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+}
+
+-(BOOL)hasEventPrefix {
+    return [self hasPrefix:[NSString stringWithFormat:@"%@%@", LDEventKeyEvent, LDEventSourceKeyValueDelimiter]]
+        || [self hasPrefix:[NSString stringWithFormat:@"%@%@", LDEventKeyData, LDEventSourceKeyValueDelimiter]]
+        || [self hasPrefix:[NSString stringWithFormat:@"%@%@", LDEventKeyId, LDEventSourceKeyValueDelimiter]]
+        || [self hasPrefix:[NSString stringWithFormat:@"%@%@", LDEventKeyRetry, LDEventSourceKeyValueDelimiter]]
+        || [self hasPrefix:LDEventSourceKeyValueDelimiter];
+}
+
+-(BOOL)hasEventTerminator {
+    return [self containsString:LDEventSourceEventTerminator];
 }
 @end
