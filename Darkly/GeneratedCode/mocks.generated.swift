a347afacc376d5d33f08feee81cbf9bc1479aca1
Mark Pokorny <markpokornycos@gmail.com>
2018-08-10 12:20:13 -0600

    remove unneeded stencil entry

--- Darkly/GeneratedCode/mocks.generated.swift
+++ Darkly/GeneratedCode/mocks.generated.swift
@@ -1,8 +1,6 @@
 // Generated using Sourcery 0.13.1 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-//swiftlint:disable vertical_whitespace
-
 
 import DarklyEventSource
 @testable import LaunchDarkly
