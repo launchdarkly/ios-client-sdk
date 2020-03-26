6c8876f291cbd3e69f07e64b389e08cbaea1a77f
torchhound <5600929+torchhound@users.noreply.github.com>
2020-03-26 03:44:48 -0700

    sed now matches specific pattern instead of line numbers

--- SourceryTemplates/remove_sourcery_duplicates.sh
+++ SourceryTemplates/remove_sourcery_duplicates.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-sed '138,147d' $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift | tee $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift
-
-#sed '/MARK: removeData/,/^$/d' $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift | tee $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift
-
-#awk 'BEGIN { del=0 } /MARK: removeData/ { del=1 } del<=0 { print } /MARK: removeData/ { del -= 1 }' $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift | tee $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift
+sed '/MARK: removeData/,/MARK: removeData/d' $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift > mock-tmp && mv mock-tmp $PROJECT_DIR/LaunchDarkly/GeneratedCode/mocks.generated.swift
