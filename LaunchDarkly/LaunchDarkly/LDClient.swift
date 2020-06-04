599c43582b21a2b81bc720451867bd854c5f62ea
torchhound <5600929+torchhound@users.noreply.github.com>
2020-06-03 18:32:17 -0700

    Changed startWaitSeconds doc string type to TimeInterval

--- LaunchDarkly/LaunchDarkly/LDClient.swift
+++ LaunchDarkly/LaunchDarkly/LDClient.swift
@@ -369,7 +369,7 @@ public class LDClient {
     
      - parameter config: The LDConfig that contains the desired configuration. (Required)
      - parameter user: The LDUser set with the desired user. If omitted, LDClient retains the previously set user, or default if one was never set. (Optional)
-     - parameter startWaitSeconds: An Int representing how long to wait for flags before returning true in the completion to indicate that it timed out.
+     - parameter startWaitSeconds: A TimeInterval representing how long to wait for flags before returning true in the completion to indicate that it timed out.
      - parameter completion: Closure called when the embedded `setOnlineIdentify` call completes, subject to throttling delays. Takes a Bool as a parameter that indicates whether the SDK did not come online within startWaitSeconds. (Optional)
      */
     public func startCompleteWhenFlagsReceived(config: LDConfig, user: LDUser? = nil, startWaitSeconds: TimeInterval, completion: ((_ timedOut: Bool) -> Void)? = nil) {
--- LaunchDarkly/LaunchDarkly/ObjectiveC/ObjcLDClient.swift
+++ LaunchDarkly/LaunchDarkly/ObjectiveC/ObjcLDClient.swift
@@ -188,7 +188,7 @@ public final class ObjcLDClient: NSObject {
 
      - parameter configWrapper: The LDConfig that contains the desired configuration. (Required)
      - parameter userWrapper: The LDUser set with the desired user. If omitted, LDClient retains the previously set user, or default if one was never set. (Optional)
-     - parameter startWaitSeconds: An Int representing how long to wait for flags before returning true in the completion to indicate that it timed out.
+     - parameter startWaitSeconds: A TimeInterval representing how long to wait for flags before returning true in the completion to indicate that it timed out.
      - parameter completion: Closure called when the embedded `setOnlineIdentify` call completes, subject to throttling delays. Takes a Bool as a parameter that indicates whether the SDK did not come online within startWaitSeconds. (Optional)
      */
     @objc public func startCompleteWhenFlagsReceived(config configWrapper: ObjcLDConfig, user userWrapper: ObjcLDUser? = nil, startWaitSeconds: TimeInterval, completion: ((_ timedOut: Bool) -> Void)? = nil) {
