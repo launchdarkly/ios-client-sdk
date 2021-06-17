2d9acce46fdc0f1a34d2c8092a5cff7392f0dde2
Gavin Whelan <gwhelan@launchdarkly.com>
2021-06-17 12:49:16 -0700

    Update dependencies. (#155)

--- Package.swift
+++ Package.swift
@@ -16,9 +16,9 @@ let package = Package(
             targets: ["LaunchDarkly"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMinor(from: "9.0.0")),
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "3.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "9.0.0")),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMinor(from: "9.1.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMinor(from: "9.2.0")),
         .package(url: "https://github.com/LaunchDarkly/swift-eventsource.git", .upToNextMinor(from: "1.2.1"))
     ],
     targets: [
