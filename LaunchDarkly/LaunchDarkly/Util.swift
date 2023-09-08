cb5bd9419062a5f02899e152643b263c4bd5330e
tanderson-ld <127344469+tanderson-ld@users.noreply.github.com>
2023-09-08 15:36:33 -0500

    Ta/sc 216351/fixing key encoding (#255)
    **Requirements**
    
    - [x] I have added test coverage for new or changed functionality
    - [x] I have followed the repository's [pull request submission
    guidelines](../blob/v6/CONTRIBUTING.md#submitting-pull-requests)
    - [x] I have validated my changes against all supported platform
    versions
    
    **Related issues**
    
    
    https://app.shortcut.com/launchdarkly/story/216351/ios-ld-application-key-is-not-base64-url-encoded
    
    **Describe the solution you've provided**
    
    Generalized util function to return Data since we already had a
    base64UrlEncoded extension method to leverage.

--- LaunchDarkly/LaunchDarkly/Util.swift
+++ LaunchDarkly/LaunchDarkly/Util.swift
@@ -6,12 +6,16 @@ class Util {
     internal static let validTagCharacterSet = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-")
 
     class func sha256base64(_ str: String) -> String {
+        sha256(str).base64EncodedString()
+    }
+    
+    class func sha256(_ str: String) -> Data {
         let data = Data(str.utf8)
         var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
         data.withUnsafeBytes {
             _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &digest)
         }
-        return Data(digest).base64EncodedString()
+        return Data(digest)
     }
 }
 
