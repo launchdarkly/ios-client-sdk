7a9918ec28346fbbab308754e32c479b460c6b5f
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-12-08 09:46:13 -0800

    extra safety for memory leak

--- LaunchDarkly/LaunchDarkly/LDClientIdentifyHook.swift
+++ LaunchDarkly/LaunchDarkly/LDClientIdentifyHook.swift
@@ -1,53 +1,54 @@
 import Foundation
-import OSLog
 
 extension LDClient {
     private struct IdentifyHookState {
         let seriesContext: IdentifySeriesContext
         let seriesData: [EvaluationSeriesData]
+        let hooksSnapshot: [Hook]
     }
     
     func executeWithIdentifyHooks(context: LDContext, work: @escaping ((@escaping (IdentifyResult) -> Void)) -> Void) {
         let state = executeBeforeIdentifyHooks(context: context)
-        work() { result in
+        work() { [weak self] result in
             guard let state else {
                 return
             }
-            self.executeAfterIdentifyHooks(context: context, state: state, result: result)
+            self?.executeAfterIdentifyHooks(state: state, result: result)
         }
     }
     
     private func executeBeforeIdentifyHooks(context: LDContext) -> IdentifyHookState? {
-        guard !self.hooks.isEmpty else {
+        guard !hooks.isEmpty else {
             return nil
         }
         
+        let hooksSnapshot = self.hooks
         let seriesContext = IdentifySeriesContext(context: context, methodName: "identify")
-        let seriesData = self.hooks.map { hook in
+        let seriesData = hooksSnapshot.map { hook in
             hook.beforeIdentify(seriesContext: seriesContext, seriesData: EvaluationSeriesData())
         }
-        return IdentifyHookState(seriesContext: seriesContext, seriesData: seriesData)
+        return IdentifyHookState(seriesContext: seriesContext, seriesData: seriesData, hooksSnapshot: hooksSnapshot)
     }
     
-    private func executeAfterIdentifyHooks(context: LDContext, state: IdentifyHookState, result: IdentifyResult) {
-        guard !self.hooks.isEmpty else {
+    private func executeAfterIdentifyHooks(state: IdentifyHookState, result: IdentifyResult) {
+        guard !state.hooksSnapshot.isEmpty else {
             return
         }
         
         // Invoke hooks in reverse order and give them back the series data they gave us.
-        zip(self.hooks, state.seriesData).reversed().forEach { (hook, data) in
+        zip(state.hooksSnapshot, state.seriesData).reversed().forEach { (hook, data) in
             _ = hook.afterIdentify(seriesContext: state.seriesContext, seriesData: data, result: result)
         }
     }
     
     func _identifyHooked(context: LDContext, sheddable: Bool, useCache: IdentifyCacheUsage, timeout: TimeInterval, completion: @escaping (_ result: IdentifyResult) -> Void) {
         if timeout > 0 {
-            self.executeWithIdentifyHooks(context: context) { hooksCompletion in
+            executeWithIdentifyHooks(context: context) { hooksCompletion in
                 TimeoutExecutor.run(
                     timeout: timeout,
                     queue: .global(),
-                    operation: { done in
-                        self._identify(context: context, sheddable: sheddable, useCache: useCache) { result in
+                    operation: { [weak self] done in
+                        self?._identify(context: context, sheddable: sheddable, useCache: useCache) { result in
                             done(result)
                         }
                     },
@@ -59,8 +60,8 @@ extension LDClient {
                 )
             }
         } else {
-            self.executeWithIdentifyHooks(context: context) { hooksCompletion in
-                self._identify(context: context, sheddable: sheddable, useCache: useCache) { result in
+            executeWithIdentifyHooks(context: context) { [weak self] hooksCompletion in
+                self?._identify(context: context, sheddable: sheddable, useCache: useCache) { result in
                     completion(result)
                     hooksCompletion(result)
                 }
