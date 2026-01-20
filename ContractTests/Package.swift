6d4d8a86355f3ded5842907d00b9d585f7674241
Todd Anderson <tanderson@launchdarkly.com>
2026-01-20 14:28:32 -0500

    bumping contract tests platform macos version

--- ContractTests/Package.swift
+++ ContractTests/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -6,7 +6,7 @@ let package = Package(
     name: "ContractTests",
     platforms: [
         .iOS(.v12),
-        .macOS(.v10_15),
+        .macOS(.v12),
         .watchOS(.v4),
         .tvOS(.v12)
     ],
