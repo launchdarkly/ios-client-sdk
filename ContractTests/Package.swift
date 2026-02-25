ed4ea8df5f43bdd89d3ce82731a0d866d7547a67
Matthew Keeler <mkeeler@launchdarkly.com>
2026-02-25 16:18:03 -0500

    match contract tests with sdk

--- ContractTests/Package.swift
+++ ContractTests/Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "ContractTests",
     platforms: [
-        .iOS(.v12),
-        .macOS(.v10_15),
-        .watchOS(.v4),
-        .tvOS(.v12)
+        .iOS(.v13),
+        .macOS(.v12),
+        .watchOS(.v6),
+        .tvOS(.v13)
     ],
     products: [
         .executable(
